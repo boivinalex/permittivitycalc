be37da0e71b1849d8495299a0fb80ba688bfb431
Alex Boivin <alex.boivin@mail.utoronto.ca>
2017-09-18 15:22:03 -0400

    Create multiple_meas fucntion which iterates through all data files in a directory and plots all the data

--- S_Param_Script.py
+++ S_Param_Script.py
@@ -1412,9 +1412,52 @@ def run_example(flag='single'):
         perm_compare(classlist)
         return test, test2, classlist
     
-def multiple_meas(file_path=None,airline=None,skip=None):
-    # Get the file path and the airline name
-    airline, file, L_in = _get_file(airline,file_path)
+def multiple_meas(file_path=None,airline=None):
+    """
+    Generate an instance of AirlineData for every file in a directory. Store \
+        the intances in a list, and plot them all using perm_compare.
+        
+    Arguments
+    ---------
+    file_path (str): Full path of any file in the source directory. \
+        (Optional - will produce file dialog box if not provided.)
+    
+    airlne (str): Name of airline used. Every measurement must have been made \
+        in the same airline. (Optional - will prompt if not provided.)
+        
+    Return
+    ------
+    class_list (lst): List of generated class instances of AirlineData.
+    """
+    # Use _get_file to get the filepath and airline name if not provided
+    if file_path:   # If file path provided as argument
+        file = file_path
+    elif not file_path:   # If file path not provided
+        print('\n')
+        print("Select any data file in the source folder. All .txt "+\
+              "files in the source folder must be METAS data tables.")
+        # Get the file path and the airline name
+        airline, file, L_in = _get_file(airline,file_path)
+    elif not airline:   # If file path is given but airline is not
+        airline, file, L_in = _get_file(airline,file_path)
+        
+    # Get directory path    
+    directory = os.path.dirname(file)
+    # Use a list to maintain order for plotting
+    class_list = []
+    # Iterate through all .txt files in the directory and run AirlineData
+    for file in os.listdir(directory):
+        if file.endswith(".txt"):
+            filename = os.path.splitext(file)[0]    # Use file name as plot label
+            # Append each new instance to class list
+            class_list.append(AirlineData(*get_METAS_data(airline,\
+                                os.path.join(directory, file)),name=filename))
+    
+    # Plot all files        
+    perm_compare(class_list)
+    
+    return class_list
+        
                 
 #%% MAIN
 def main():

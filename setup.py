4916397e17e745a625f9aef9388dfab74c007e3d
Alex Boivin <alex.boivin@mail.utoronto.ca>
2018-02-22 18:34:40 -0500

    Fix module references

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='permittivitycalc',  
 
-    version='0.1.0',
+    version='0.1.1',
 
     description='Scripts to calculate and plot the complex permittivity from S-parameter data',  
 

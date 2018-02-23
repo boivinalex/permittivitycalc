fc30c517d30d60540cf68371d21f3ea7c3f34894
Alex Boivin <alex.boivin@mail.utoronto.ca>
2018-02-22 19:16:10 -0500

    Move data folder

--- permittivitycalc/run_permittivity_calc.py
+++ permittivitycalc/run_permittivity_calc.py
@@ -14,7 +14,7 @@ import uncertainties
 from uncertainties import unumpy as unp
 
 # Get data folder path for example files
-PACKAGEPATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PACKAGEPATH = os.path.dirname(os.path.abspath(__file__))
 DATAPATH = os.path.join(PACKAGEPATH, 'data')  
 
         

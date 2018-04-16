dfa3e9fccd4e80cc25cb5e8fc811e9a48809600c
Alex Boivin <alex.boivin@mail.utoronto.ca>
2018-04-16 13:18:14 -0400

    Delete build files

--- build/lib/permittivitycalc/__init__.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import uncertainties 
-#Citation: Uncertainties: a Python package for calculations with uncertainties,
-#    Eric O. LEBIGOT, http://pythonhosted.org/uncertainties/
-from uncertainties import unumpy as unp
-
-from .sparam_data import AirlineData, pplot, get_METAS_data, perm_compare, multiple_meas, run_default, run_example

3a1ee531abd541d6e0f07321c9675bb916dcd8f1
Alex Boivin <alex.boivin@mail.utoronto.ca>
2018-10-23 12:10:46 -0400

    Deal with no display issue

--- permittivitycalc/__init__.py
+++ permittivitycalc/__init__.py
@@ -1,3 +1,8 @@
 from .sparam_data import AirlineData, multiple_meas, run_default, run_example
 from . import permittivity_plot as pplot
 from .helper_functions import get_METAS_data, perm_compare
+
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    import matplotlib
+    matplotlib.use('Agg')

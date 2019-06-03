fa00cb8adb67cdd0cd7dab984cf284adaf31f2b5
Alex Boivin <alex.boivin@mail.utoronto.ca>
2019-06-03 16:23:07 -0400

    Version 0.5.1.post1 - fix travisci deploy

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='permittivitycalc',  
 
-    version='0.5.1',
+    version='0.5.1.post1',
 
     description='Scripts to calculate and plot the complex permittivity from S-parameter data',  
 

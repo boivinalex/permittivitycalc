3088e77aa89c5d8fb0c3591b7b721be1dd9ad3f9
Alex Boivin <alex.boivin@mail.utoronto.ca>
2019-10-03 15:24:04 -0400

    ipython work around

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ extensions = ['sphinx.ext.autodoc',
 	'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'IPython.sphinxext.ipython_console_highlighting'
 ]
 
 # Add any paths that contain templates here, relative to this directory.

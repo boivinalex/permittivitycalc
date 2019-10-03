9de67912700c9abe77934f68f4d09ca81f831f1b
Alex Boivin <alex.boivin@mail.utoronto.ca>
2019-10-03 15:27:37 -0400

    ipython work around

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
+    'IPython.sphinxext.ipython_directive',
     'IPython.sphinxext.ipython_console_highlighting'
 ]
 
